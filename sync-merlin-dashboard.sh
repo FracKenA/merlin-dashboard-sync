32376ec0d5b9249c7e94d06ea5594fefcfa76910
Ken D <kdobbins@op5.com>
2018-11-13 20:39:16 -0600

    added logic to sync

--- sync-merlin-dashboard.sh
+++ sync-merlin-dashboard.sh
@@ -44,6 +44,50 @@ for node in $(mon node list --type=peer); \
     done;
 }
 
+compare_remote_database_checksum_and_sync ()
+{
+for node in $(mon node list --type=peer); \
+    do
+        local CHECKSUM_REMOTE
+        CHECKSUM_REMOTE=$(asmonitor \
+            ssh "$node" \
+            'mysql -u root -e "\
+            checksum table \
+            merlin.dashboards, \
+            merlin.dashboard_widgets, \
+            merlin.ninja_report_comments, \
+            merlin.custom_vars, \
+            merlin.ninja_saved_filters, \
+            merlin.ninja_settings, \
+            merlin.permission_quarks, \
+            merlin.saved_reports, \
+            merlin.saved_reports_objects, \
+            merlin.saved_reports_options \
+            "')
+        CHECKSUM_LOCAL=$(asmonitor \
+            mysql -u root -e " \
+            checksum table \
+            merlin.dashboards, \
+            merlin.dashboard_widgets, \
+            merlin.ninja_report_comments, \
+            merlin.custom_vars, \
+            merlin.ninja_saved_filters, \
+            merlin.ninja_settings, \
+            merlin.permission_quarks, \
+            merlin.saved_reports, \
+            merlin.saved_reports_objects, \
+            merlin.saved_reports_options \
+            ")
+        IF [[ "$CHECKSUM_LOCAL" == "$CHECKSUM_REMOTE" ]]; 
+            then
+                # echo "$node sync succeeded" >> /opt/monitor/var/merlin_database_sync.log
+
+            else
+                export_sql_tables
+                sync_and_import_sql_file_to_peers
+        FI
+    done;
+}
 
 compare_remote_database_checksum ()
 {
@@ -87,77 +131,3 @@ for node in $(mon node list --type=peer); \
         FI
     done;
 }
-
-
-export_sql_tables
-sync_and_import_sql_file_to_peers
-compare_remote_database_checksum
-
-
-##
-# Removed to make single ssh call
-##
-# sync_and_import_sql_file_to_peers ()
-# {
-# for node in $(mon node list --type=peer); \
-#     do asmonitor \
-#         scp /opt/monitor/var/merlin_database_sync.sql \
-#             "$node":/opt/monitor/var/merlin_database_sync.sql && \
-#     done;
-# }
-
-##
-# Removed to combine sync and import into single process.
-##
-# import_sql_file_on_peers ()
-# {
-# for node in $(mon node list --type=peer); \
-#     do asmonitor \
-#         ssh "$node" 'mysql -u root merlin < /opt/monitor/var/merlin_database_sync.sql';
-#     done;
-# }
-
-##
-# Removed to test checksum alternative
-##
-# dump_remote_database_tables ()
-# {
-# for node in $(mon node list --type=peer); \
-#     do asmonitor \
-#         mysqldump -u root --add-drop-table merlin \
-#             dashboards \
-#             dashboard_widgets \
-#             ninja_report_comments \
-#             custom_vars \
-#             ninja_saved_filters \
-#             ninja_settings \
-#             permission_quarks \
-#             saved_reports \
-#             saved_reports_objects \
-#             saved_reports_options \
-#         > /opt/monitor/var/merlin_database_sync_verify.sql;
-#     done;
-# }
-
-##
-#Removed to test checksum alternative
-##
-# compare_remote_database ()
-# {
-# for node in $(mon node list --type=peer); \
-#     do
-#         local OUTPUT
-#         OUTPUT=$(asmonitor ssh "$node" 'cmp -s /opt/monitor/var/merlin_database_sync.sql /opt/monitor/var/merlin_database_sync_verify.sql')
-#         IF [[ "$OUTPUT" -eq "0" ]]; 
-#             then
-#                 echo "$node sync succeeded" >> /opt/monitor/var/merlin_database_sync.log
-#             else
-#                 echo "$node sync failed" >> /opt/monitor/var/merlin_database_sync.log
-#         FI
-#     done;
-# }
-
-# sync_sql_file_to_peers
-# import_sql_file_on_peers
-# dump_remote_database_tables
-# compare_remote_database
